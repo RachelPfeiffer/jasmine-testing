f4e0a5236a574018f5caf2f50a5be47ef7b0a363
RachelPfeiffer <42552533+RachelPfeiffer@users.noreply.github.com>
2018-12-10 22:09:54 +0200

    wrote menu test suite

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,28 +31,66 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+        function isURLDefined(feed) {
+          it('should have url defined', function () {
+            expect(feed.url).toBeDefined();
+            expect(feed.url.length).toBeGreaterThan(0);
+          });
+        }
 
+        for(eachFeed of allFeeds) {
+          isURLDefined(eachFeed);
+        }
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+
+         function isNameDefined(feed) {
+           it('should have name defined', function () {
+             expect(feed.name).toBeDefined();
+             expect(feed.name.length).toBeGreaterThan(0);
+           });
+         }
+
+         for(eachFeed of allFeeds) {
+           isNameDefined(eachFeed);
+         }
     });
 
 
     /* TODO: Write a new test suite named "The menu" */
-
+$(function() {
+  describe('The Menu', function() {
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+         it('is hidden by default', function() {
+           expect(document.body).toHaveClass('menu-hidden');
+});
+        /* TODO: Write a test that ensures the menu changes
+         * visibility when the menu icon is clicked. This test
+         * should have two expectations: does the menu display when
+         * clicked and does it hide when clicked again.
+         */
 
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
+ const hamburger = document.querySelector('.menu-icon-link');
+
+         it('displays when menu icon is clicked', function() {
+           console.log(hamburger);
+           hamburger.click();
+           expect(document.body).not.toHaveClass('menu-hidden');
+         });
+
+         it('hides when menu icon is clicked again', function () {
+           hamburger.click();
+           expect(document.body).toHaveClass('menu-hidden');
+         })
+})
+});
 
     /* TODO: Write a new test suite named "Initial Entries" */
 
