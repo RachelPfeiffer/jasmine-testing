5e36888139b577079b35758c29f79a2627992df4
RachelPfeiffer <42552533+RachelPfeiffer@users.noreply.github.com>
2018-12-12 10:34:24 +0200

    made test for whether feed is empty more specific per grader

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -27,7 +27,7 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
+        /* loop through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
@@ -42,7 +42,7 @@ $(function() {
           isURLDefined(eachFeed);
         }
 
-        /* TODO: Write a test that loops through each feed
+        /* loop through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
@@ -60,10 +60,9 @@ $(function() {
     });
 
 
-    /* TODO: Write a new test suite named "The menu" */
 const menu = function() {
   describe('The Menu', function() {
-        /* TODO: Write a test that ensures the menu element is
+        /* test that the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
@@ -71,7 +70,7 @@ const menu = function() {
          it('is hidden by default', function() {
            expect(document.body).toHaveClass('menu-hidden');
 });
-        /* TODO: Write a test that ensures the menu changes
+        /* test that the menu changes
          * visibility when the menu icon is clicked. This test
          * should have two expectations: does the menu display when
          * clicked and does it hide when clicked again.
@@ -91,10 +90,9 @@ const menu = function() {
 
 menu();
 
-    /* TODO: Write a new test suite named "Initial Entries" */
 const initialEntries = function() {
   describe('Initial Entries', function() {
-    /* TODO: Write a test that ensures when the loadFeed
+    /* test that when the loadFeed
      * function is called and completes its work, there is at least
      * a single .entry element within the .feed container.
      * Remember, loadFeed() is asynchronous so this test will require
@@ -106,14 +104,13 @@ const initialEntries = function() {
      });
 
      it('contains at least one blog post', function () {
-       const grabAnEntry = document.querySelectorAll('.entry');
-       expect(grabAnEntry).not.toBe(null);
+       const grabAnEntry = document.querySelectorAll('.feed .entry');
+       expect(grabAnEntry).length.toBeGreaterThan(0);
      });
 };
 initialEntries();
 
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
 const newFeedSelection = function() {
   let initialFeedText;
   let updatedFeedText;
@@ -141,8 +138,6 @@ const newFeedSelection = function() {
 });
 }
 newFeedSelection();
-        /* TODO: Write a test that ensures when a new feed is loaded
-         * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
-         */
+
+
 }());
